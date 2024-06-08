ec4d4e9bf30b42b812b672b4d1fa253ca38ce8f8
F-Zaman-Rafi <gollarafi@gmail.com>
2024-06-08 18:20:32 +0800

    reorganise the index file

--- index.js
+++ index.js
@@ -1,65 +1,126 @@
 const express = require('express')
-const cors = require('cors');
-const app = express();
+const app = express()
 require('dotenv').config()
-const port = process.env.PORT || 5000;
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const jwt = require('jsonwebtoken')
+
+const port = process.env.PORT || 8000
 
 // middleware
-app.use(cors());
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+
 app.use(express.json())
+app.use(cookieParser())
 
-// MONGO CONFIGS
+// Verify Token Middleware
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token
+    console.log(token)
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            console.log(err)
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded
+        next()
+    })
+}
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-esohqc9-shard-00-00.okia5sv.mongodb.net:27017,ac-esohqc9-shard-00-01.okia5sv.mongodb.net:27017,ac-esohqc9-shard-00-02.okia5sv.mongodb.net:27017/?ssl=true&replicaSet=atlas-mrsszx-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-esohqc9-shard-00-00.okia5sv.mongodb.net:27017,ac-esohqc9-shard-00-01.okia5sv.mongodb.net:27017,ac-esohqc9-shard-00-02.okia5sv.mongodb.net:27017/?ssl=true&replicaSet=atlas-mrsszx-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    }
-});
+    },
+})
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        const sessionCollection = client.db('ThinkSyncDB').collection('StudySession')
+        // auth related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '365d',
+            })
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                })
+                .send({ success: true })
+        })
+        // Logout
+        app.get('/logout', async (req, res) => {
+            try {
+                res
+                    .clearCookie('token', {
+                        maxAge: 0,
+                        secure: process.env.NODE_ENV === 'production',
+                        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                    })
+                    .send({ success: true })
+                console.log('Logout successful')
+            } catch (err) {
+                res.status(500).send(err)
+            }
+        })
 
-        const sessionCollection = client.db("ThinkSyncDB").collection("StudySession");
-        const reviewCollection = client.db("ThinkSyncDB").collection("reviews");
-
+        // get sessions data from db
         app.get('/sessions', async (req, res) => {
             const result = await sessionCollection.find().toArray();
             res.send(result)
-        })
 
-        app.get('/reviews', async (req, res) => {
-            const result = await reviewCollection.find().toArray();
-            res.send(result);
         })
 
 
+
+
+        // Get all rooms from db
+        // app.get('/rooms', async (req, res) => {
+        //     const category = req.query.category
+        //     console.log(category)
+        //     let query = {}
+        //     if (category && category !== 'null') query = { category }
+        //     const result = await roomsCollection.find(query).toArray()
+        //     res.send(result)
+        // })
+
+        // Get a single room data from db using _id
+        // app.get('/room/:id', async (req, res) => {
+        //     const id = req.params.id
+        //     const query = { _id: new ObjectId(id) }
+        //     const result = await roomsCollection.findOne(query)
+        //     res.send(result)
+        // })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        await client.db('admin').command({ ping: 1 })
+        console.log(
+            'Pinged your deployment. You successfully connected to MongoDB!'
+        )
     } finally {
         // Ensures that the client will close when you finish/error
-        // await client.close();
     }
 }
-run().catch(console.dir);
-
-
-
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
-    res.send('boss is sitting')
+    res.send('Hello from StayVista Server..')
 })
 
 app.listen(port, () => {
-    console.log(`ThinkSync Server is ready to bang on port ${port}`);
+    console.log(`StayVista is running on port ${port}`)
 })

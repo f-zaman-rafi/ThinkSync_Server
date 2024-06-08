99b88c5ca697167f25dd5d49bac1d37079b44bce
F-Zaman-Rafi <gollarafi@gmail.com>
2024-06-08 18:28:14 +0800

    getting single data from sessionDB

--- index.js
+++ index.js
@@ -85,6 +85,14 @@ async function run() {
 
         })
 
+        // get single sesssion from db using its _id
+        app.get('/sessions/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await sessionCollection.findOne(query)
+            res.send(result)
+        })
+
 
 
 

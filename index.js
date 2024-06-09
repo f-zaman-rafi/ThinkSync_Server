df05be318789485feae02b4b444485026b78eda1
F-Zaman-Rafi <gollarafi@gmail.com>
2024-06-10 03:43:10 +0800

    getting all user data from database

--- index.js
+++ index.js
@@ -90,7 +90,21 @@ async function run() {
 
         app.post('/users', async (req, res) => {
             const user = req.body;
+
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+
             const result = await userCollection.insertOne(user);
+            res.send(result)
+        })
+
+        // get all users
+
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
             res.send(result)
         })
 
